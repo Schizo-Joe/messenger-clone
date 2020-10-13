c084097a82ef2840590a0424810da4894c2d2098
Chris Gruber <chris@chrisgruber.com>
2020-10-12 20:14:32 -0500

    formatted remaining documents with Prettier

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { AuthProvider } from '../contexts/AuthContext';
-import Home from './Home';
-import Login from './Login';
-import PrivateRoute from './PrivateRoute';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { AuthProvider } from "../contexts/AuthContext";
+import Home from "./Home";
+import Login from "./Login";
+import PrivateRoute from "./PrivateRoute";
 
 const App = () => {
   return (
     <Router>
       <AuthProvider>
         <Switch>
-          <PrivateRoute exact path="/" component={ Home } />
-          <Route path="/login" component={ Login } />
+          <PrivateRoute exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
         </Switch>
       </AuthProvider>
     </Router>
